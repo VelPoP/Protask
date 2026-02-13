89aa67168fdd0f7bb98814ee15ac22fa6af528b5
VelPoP <kathir1441v5@gmail.com>
2026-02-13 10:59:40 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,20 +1,38 @@
-
 self.addEventListener('install', (event) => {
+    console.log('[SW] Installing...');
     self.skipWaiting();
 });
 
 self.addEventListener('activate', (event) => {
+    console.log('[SW] Activating...');
     event.waitUntil(clients.claim());
 });
 
 self.addEventListener('message', (event) => {
+    console.log('[SW] Message received:', event.data);
+    
     if (event.data.type === 'SHOW_NOTIFICATION') {
         self.registration.showNotification(event.data.title, {
             body: event.data.body,
             icon: 'https://cdn-icons-png.flaticon.com/512/906/906334.png',
+            badge: 'https://cdn-icons-png.flaticon.com/512/906/906334.png',
             vibrate: [200, 100, 200],
-            tag: 'task-alert'
+            tag: 'task-alert',
+            requireInteraction: true,
+            actions: [
+                { action: 'complete', title: 'Mark Complete' },
+                { action: 'snooze', title: 'Snooze 30min' }
+            ]
         });
     }
 });
 
+self.addEventListener('notificationclick', (event) => {
+    console.log('[SW] Notification clicked:', event.action);
+    event.notification.close();
+    
+    // Open the app when notification is clicked
+    event.waitUntil(
+        clients.openWindow('/')
+    );
+});
